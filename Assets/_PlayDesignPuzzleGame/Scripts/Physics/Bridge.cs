f6a54ec8805aaad63abb83d797cb7bd25b6fbd56
SebastianSwarney <34040389+SebastianSwarney@users.noreply.github.com>
2020-03-22 14:06:27 -0400

    Added Aura

---
+++ Assets/_PlayDesignPuzzleGame/Scripts/Physics/Bridge.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+	public Transform m_startNode;
+
+	public Transform m_endNode;
+
+	public PushableObject m_targetObject;
+}
